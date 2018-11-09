e28806fae67801f799193b5aa9c599a72c579dbf
arjunchawla <32623416+arjunchawla@users.noreply.github.com>
2018-11-08 20:34:43 -0800

    Update ScheduleDetailsViewController.swift

--- ScheduleDetailsViewController.swift
+++ ScheduleDetailsViewController.swift
@@ -15,11 +15,17 @@ class ScheduleDetailsViewController: UIViewController {
     @IBOutlet weak var durationTextField: UITextField!
     @IBOutlet weak var repeatTextField: UITextField!
     @IBOutlet weak var repeatSwitch: UISwitch!
-    
+    //var to manipulate the starting time
+    @IBOutlet weak var startingTime: UITextField!
+    //var to manipulate the duration
+    @IBOutlet weak var duration: UITextField!
+    let datePicker = UIDatePicker()
+    let datePicker2 = UIDatePicker()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        createDatePicker1()
+        createDatePicker2()
     }
 
 //    override func didReceiveMemoryWarning() {
@@ -27,15 +33,70 @@ class ScheduleDetailsViewController: UIViewController {
 //        // Dispose of any resources that can be recreated.
 //    }
     
+    //functiong to create the date picker when the text field is clicked
+    func createDatePicker1(){
+        
+        datePicker.datePickerMode = .time
+        startingTime.inputView = datePicker
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        
+        
+        
+        
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneClicked))
+        toolBar.setItems([doneButton], animated: true)
+        
+        startingTime.inputAccessoryView = toolBar
+        
+
+        
+    }
+    
+    func createDatePicker2(){
+        datePicker2.datePickerMode = .countDownTimer
+        duration.inputView = datePicker2
+        let toolBar2 = UIToolbar()
+        toolBar2.sizeToFit()
+        
+        let doneButton2 = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(done2Clicked))
+        toolBar2.setItems([doneButton2], animated: true)
+        
+        duration.inputAccessoryView = toolBar2
+        
+    }
+    
+    @objc func doneClicked(){
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeStyle = .short
+    dateFormatter.dateStyle = .none
+        
+        startingTime.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func done2Clicked(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .none
+        
+        
+        
+        duration.text = "\(datePicker2.countDownDuration) secs"
+        self.view.endEditing(true)
+    }
+    
     @IBAction func unwindSegue(_sender: UIStoryboard) {
         
     }
     
-    //added the date picker to pick the starting time
-    @IBOutlet weak var startingTime: UIDatePicker!
+
+
     
-    //added the timer for duration
-    @IBOutlet weak var durationTimer: UIDatePicker!
+    
+
     
     
     
